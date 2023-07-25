bade7dd3a157427f25971497555931c2bb2608f0
Navid-Derakhshandeh <111235264+Navid-Derakhshandeh@users.noreply.github.com>
2023-07-25 15:24:20 +0330

    Attend Version 1.0.02

---
+++ Attend  V 1.0.02/Attend/Main.Designer.cs
@@ -0,0 +1,199 @@
+﻿
+namespace Attend
+{
+    partial class Main
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Main));
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.inventoryManagementSystemToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.iMSToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.hotelManagementSystemToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.manageRoomToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.manageReservationsToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.manageClientsToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.windowsToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.cascadeToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.tileVerticalToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.tileHorizontalToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.financialManagementToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.financialIncomeToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.financialIncomeGraphToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.inventoryManagementSystemToolStripMenuItem,
+            this.hotelManagementSystemToolStripMenuItem,
+            this.windowsToolStripMenuItem,
+            this.financialManagementToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(800, 24);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // inventoryManagementSystemToolStripMenuItem
+            // 
+            this.inventoryManagementSystemToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.iMSToolStripMenuItem});
+            this.inventoryManagementSystemToolStripMenuItem.Name = "inventoryManagementSystemToolStripMenuItem";
+            this.inventoryManagementSystemToolStripMenuItem.Size = new System.Drawing.Size(184, 20);
+            this.inventoryManagementSystemToolStripMenuItem.Text = "Inventory Management System";
+            // 
+            // iMSToolStripMenuItem
+            // 
+            this.iMSToolStripMenuItem.Name = "iMSToolStripMenuItem";
+            this.iMSToolStripMenuItem.Size = new System.Drawing.Size(94, 22);
+            this.iMSToolStripMenuItem.Text = "IMS";
+            this.iMSToolStripMenuItem.Click += new System.EventHandler(this.iMSToolStripMenuItem_Click);
+            // 
+            // hotelManagementSystemToolStripMenuItem
+            // 
+            this.hotelManagementSystemToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.manageRoomToolStripMenuItem,
+            this.manageReservationsToolStripMenuItem,
+            this.manageClientsToolStripMenuItem});
+            this.hotelManagementSystemToolStripMenuItem.Name = "hotelManagementSystemToolStripMenuItem";
+            this.hotelManagementSystemToolStripMenuItem.Size = new System.Drawing.Size(163, 20);
+            this.hotelManagementSystemToolStripMenuItem.Text = "Hotel Management System";
+            // 
+            // manageRoomToolStripMenuItem
+            // 
+            this.manageRoomToolStripMenuItem.Name = "manageRoomToolStripMenuItem";
+            this.manageRoomToolStripMenuItem.Size = new System.Drawing.Size(193, 22);
+            this.manageRoomToolStripMenuItem.Text = "Manage Room";
+            this.manageRoomToolStripMenuItem.Click += new System.EventHandler(this.manageRoomToolStripMenuItem_Click);
+            // 
+            // manageReservationsToolStripMenuItem
+            // 
+            this.manageReservationsToolStripMenuItem.Name = "manageReservationsToolStripMenuItem";
+            this.manageReservationsToolStripMenuItem.Size = new System.Drawing.Size(193, 22);
+            this.manageReservationsToolStripMenuItem.Text = "Manage Reservationns";
+            this.manageReservationsToolStripMenuItem.Click += new System.EventHandler(this.manageReservationsToolStripMenuItem_Click);
+            // 
+            // manageClientsToolStripMenuItem
+            // 
+            this.manageClientsToolStripMenuItem.Name = "manageClientsToolStripMenuItem";
+            this.manageClientsToolStripMenuItem.Size = new System.Drawing.Size(193, 22);
+            this.manageClientsToolStripMenuItem.Text = "Manage Clients";
+            this.manageClientsToolStripMenuItem.Click += new System.EventHandler(this.manageClientsToolStripMenuItem_Click);
+            // 
+            // windowsToolStripMenuItem
+            // 
+            this.windowsToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.cascadeToolStripMenuItem,
+            this.tileVerticalToolStripMenuItem,
+            this.tileHorizontalToolStripMenuItem});
+            this.windowsToolStripMenuItem.Name = "windowsToolStripMenuItem";
+            this.windowsToolStripMenuItem.Size = new System.Drawing.Size(68, 20);
+            this.windowsToolStripMenuItem.Text = "Windows";
+            // 
+            // cascadeToolStripMenuItem
+            // 
+            this.cascadeToolStripMenuItem.Name = "cascadeToolStripMenuItem";
+            this.cascadeToolStripMenuItem.Size = new System.Drawing.Size(150, 22);
+            this.cascadeToolStripMenuItem.Text = "Cascade";
+            this.cascadeToolStripMenuItem.Click += new System.EventHandler(this.cascadeToolStripMenuItem_Click);
+            // 
+            // tileVerticalToolStripMenuItem
+            // 
+            this.tileVerticalToolStripMenuItem.Name = "tileVerticalToolStripMenuItem";
+            this.tileVerticalToolStripMenuItem.Size = new System.Drawing.Size(150, 22);
+            this.tileVerticalToolStripMenuItem.Text = "Tile Vertical";
+            this.tileVerticalToolStripMenuItem.Click += new System.EventHandler(this.tileVerticalToolStripMenuItem_Click);
+            // 
+            // tileHorizontalToolStripMenuItem
+            // 
+            this.tileHorizontalToolStripMenuItem.Name = "tileHorizontalToolStripMenuItem";
+            this.tileHorizontalToolStripMenuItem.Size = new System.Drawing.Size(150, 22);
+            this.tileHorizontalToolStripMenuItem.Text = "Tile Horizontal";
+            this.tileHorizontalToolStripMenuItem.Click += new System.EventHandler(this.tileHorizontalToolStripMenuItem_Click);
+            // 
+            // financialManagementToolStripMenuItem
+            // 
+            this.financialManagementToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.financialIncomeToolStripMenuItem,
+            this.financialIncomeGraphToolStripMenuItem});
+            this.financialManagementToolStripMenuItem.Name = "financialManagementToolStripMenuItem";
+            this.financialManagementToolStripMenuItem.Size = new System.Drawing.Size(140, 20);
+            this.financialManagementToolStripMenuItem.Text = "Financial Management";
+            // 
+            // financialIncomeToolStripMenuItem
+            // 
+            this.financialIncomeToolStripMenuItem.Name = "financialIncomeToolStripMenuItem";
+            this.financialIncomeToolStripMenuItem.Size = new System.Drawing.Size(199, 22);
+            this.financialIncomeToolStripMenuItem.Text = "Financial Income";
+            this.financialIncomeToolStripMenuItem.Click += new System.EventHandler(this.financialIncomeToolStripMenuItem_Click);
+            // 
+            // financialIncomeGraphToolStripMenuItem
+            // 
+            this.financialIncomeGraphToolStripMenuItem.Name = "financialIncomeGraphToolStripMenuItem";
+            this.financialIncomeGraphToolStripMenuItem.Size = new System.Drawing.Size(199, 22);
+            this.financialIncomeGraphToolStripMenuItem.Text = "Financial Income Graph";
+            this.financialIncomeGraphToolStripMenuItem.Click += new System.EventHandler(this.financialIncomeGraphToolStripMenuItem_Click);
+            // 
+            // Main
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.menuStrip1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.IsMdiContainer = true;
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Main";
+            this.Text = "Attend";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem inventoryManagementSystemToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem iMSToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem windowsToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem cascadeToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem tileVerticalToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem tileHorizontalToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem hotelManagementSystemToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem manageRoomToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem manageReservationsToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem manageClientsToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem financialManagementToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem financialIncomeToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem financialIncomeGraphToolStripMenuItem;
+    }
+}
+
---
+++ Attend  V 1.0.02/Attend/Main.cs
@@ -0,0 +1,78 @@
+﻿//Navid-Derakhshandeh
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Attend
+{
+    public partial class Main : Form
+    {
+        public Main()
+        {
+            InitializeComponent();
+        }
+
+        private void iMSToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            IMS frm = new IMS();
+            frm.MdiParent = this;
+            frm.Show();
+        }
+
+        private void cascadeToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            LayoutMdi(MdiLayout.Cascade);
+        }
+
+        private void tileVerticalToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            LayoutMdi(MdiLayout.TileVertical);
+        }
+
+        private void tileHorizontalToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            LayoutMdi(MdiLayout.TileHorizontal);
+        }
+
+        private void manageRoomToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            ManageRoom frm = new ManageRoom();
+            frm.MdiParent = this;
+            frm.Show();
+        }
+
+        private void manageReservationsToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            ManageReserv frm = new ManageReserv();
+            frm.MdiParent = this;
+            frm.Show();
+        }
+
+        private void manageClientsToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            MAC frm = new MAC();
+            frm.MdiParent = this;
+            frm.Show();
+        }
+
+        private void financialIncomeToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Fininc frm = new Fininc();
+            frm.MdiParent = this;
+            frm.Show();
+        }
+
+        private void financialIncomeGraphToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            FinincGraph frm = new FinincGraph();
+            frm.MdiParent = this;
+            frm.Show();
+        }
+    }
+}
