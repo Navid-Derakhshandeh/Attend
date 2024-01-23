79f0b25be9abda927493ba52c841efd63de9718e
Navid-Derakhshandeh <111235264+Navid-Derakhshandeh@users.noreply.github.com>
2024-01-24 01:41:11 +0330

    Update Feature Improved

---
+++ Attend  V 1.0.05/Attend/IMS.cs
@@ -0,0 +1,284 @@
+﻿//Navid-Derakhshandeh
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Diagnostics;
+using System.Data.SqlClient;
+using System.Windows.Forms;
+using System.IO;
+using Microsoft.Office.Interop.Excel;
+using Microsoft.Office.Interop.Word;
+
+
+namespace Attend
+{
+    public partial class IMS : Form
+    {
+        string connString = @"Data Source=DESKTOP-21H6HAU\DESKTOP;Initial Catalog=Attend;Integrated Security=True;
+            Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False";
+
+        SqlDataAdapter dataAdapter;
+        System.Data.DataTable table;
+        SqlConnection conn;
+        string selectionStatement = "Select * from Inventory";
+        public IMS()
+        {
+            InitializeComponent();
+        }
+
+        private void IMS_Load(object sender, EventArgs e)
+        {
+            cboSearch.SelectedIndex = 0;
+            dataGridView1.DataSource = bindingSource1;
+            GetData(selectionStatement);
+        }
+        private void GetData(string selectCommand)
+        {
+            try
+            {
+                dataAdapter = new SqlDataAdapter(selectCommand, connString);
+                table = new System.Data.DataTable();
+                table.Locale = System.Globalization.CultureInfo.InvariantCulture;
+                dataAdapter.Fill(table);
+                bindingSource1.DataSource = table;
+                dataGridView1.Columns[0].ReadOnly = true;
+
+            }
+            catch (SqlException ex)
+            {
+                MessageBox.Show(ex.Message);
+
+            }
+            
+        }
+
+        private void btnAdd_Click(object sender, EventArgs e)
+        {
+            SqlCommand command;
+            string insert = @"insert into Inventory(Date_Added, Name, Manufacture, Model_Name, Quantity, Cost_Price, Sell_Price,
+                                                      Location, Condition, Availabel, Allocated, Suppliers, Notes, Category, Serial_Number, Image)
+
+                               values(@Date_Added, @Name, @Manufacture, @Model_Name, @Quantity, @Cost_Price, @Sell_Price,
+                                                      @Location, @Condition, @Availabel, @Allocated, @Suppliers, @Notes, @Category, @Serial_Number, @Image)";
+            using (conn = new SqlConnection(connString))
+            {
+                try
+                {
+                    conn.Open();
+                    command = new SqlCommand(insert, conn);
+                    command.Parameters.AddWithValue(@"Date_Added", dateTimePicker1.Value.Date);
+                    command.Parameters.AddWithValue(@"Name", txtName.Text);
+                    command.Parameters.AddWithValue(@"Model_Name", txtModel.Text); ;
+                    command.Parameters.AddWithValue(@"Quantity", txtQuantity.Text);
+                    command.Parameters.AddWithValue(@"Cost_Price", txtCost.Text);
+                    command.Parameters.AddWithValue(@"Sell_Price", txtSell.Text);
+                    command.Parameters.AddWithValue(@"Location", txtLoc.Text);
+                    command.Parameters.AddWithValue(@"Condition", txtC.Text);
+                    command.Parameters.AddWithValue(@"Manufacture", txtM.Text);
+                    command.Parameters.AddWithValue(@"Availabel", txtAV.Text);
+                    command.Parameters.AddWithValue(@"Allocated", txtAL.Text);
+                    command.Parameters.AddWithValue(@"Suppliers", txtS.Text);
+                    command.Parameters.AddWithValue(@"Serial_Number", txtSerial.Text);
+                    command.Parameters.AddWithValue(@"Category", txtCa.Text);
+                    command.Parameters.AddWithValue(@"Notes", txtNotes.Text);
+                    if (dlgOI.FileName != "")
+                        command.Parameters.AddWithValue(@"Image", File.ReadAllBytes(dlgOI.FileName));
+                    else
+                        command.Parameters.Add("@Image", SqlDbType.VarBinary).Value = DBNull.Value;
+                    
+                    command.ExecuteNonQuery();
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message);
+
+                }
+
+
+            }
+            GetData(selectionStatement);
+            dataGridView1.Update();
+        }
+
+        private void dataGridView1_CellEndEdit(object sender, DataGridViewCellEventArgs e)
+        {
+            SqlCommandBuilder commandBuilder = new SqlCommandBuilder(dataAdapter);
+            dataAdapter.UpdateCommand = commandBuilder.GetUpdateCommand();
+            try
+            {
+                bindingSource1.EndEdit();
+                dataAdapter.Update(table);
+                MessageBox.Show("Update Successfull");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+
+        }
+
+        private void btnDelete_Click(object sender, EventArgs e)
+        {
+            DataGridViewRow row = dataGridView1.CurrentCell.OwningRow;
+            string value = row.Cells["ID"].Value.ToString();
+            string Pname = row.Cells["Name"].Value.ToString();
+            string quantity = row.Cells["Quantity"].Value.ToString();
+            DialogResult result = MessageBox.Show("Do You Want to Delete This Record " + Pname + "" + quantity + ", record " + value, "Message",
+                MessageBoxButtons.YesNo, MessageBoxIcon.Question);
+
+            string deleteState = @"Delete from Inventory where id = '" + value + " ' ";
+
+            if (result == DialogResult.Yes)
+            {
+                using (conn = new SqlConnection(connString))
+                {
+                    try
+                    {
+                        conn.Open();
+                        SqlCommand comm = new SqlCommand(deleteState, conn);
+                        comm.ExecuteNonQuery();
+                        GetData(selectionStatement);
+                        dataGridView1.Update();
+                    }
+                    catch (Exception ex)
+                    {
+                        MessageBox.Show(ex.Message);
+                    }
+                }
+            }
+        }
+
+        private void btnSearch_Click(object sender, EventArgs e)
+        {
+            switch (cboSearch.SelectedItem.ToString())
+            {
+                case "Name":
+                    GetData("select * from Inventory where lower(name) like '%" + txtSearch.Text.ToLower() + "%'");
+                    break;
+                case "Manufacture":
+                    GetData("select * from Inventory where lower(manufacture) like '%" + txtSearch.Text.ToLower() + "%'");
+                    break;
+                case "Suppliers":
+                    GetData("select * from Inventory where lower(suppliers) like '%" + txtSearch.Text.ToLower() + "%'");
+                    break;
+            }
+        }
+
+        private void btnGetImage_Click(object sender, EventArgs e)
+        {
+            if (dlgOI.ShowDialog() == DialogResult.OK)
+                pictureBox1.Load(dlgOI.FileName);
+        }
+
+        private void pictureBox1_DoubleClick(object sender, EventArgs e)
+        {
+            Form frm = new Form();
+            frm.BackgroundImage = pictureBox1.Image;
+            frm.Size = pictureBox1.Image.Size;
+            frm.Show();
+        }
+
+        private void btnSaveToText_Click(object sender, EventArgs e)
+        {
+            if (saveFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                using (StreamWriter sw = new StreamWriter(saveFileDialog1.FileName))
+                {
+                    foreach (DataGridViewRow row in dataGridView1.Rows)
+                    {
+                        foreach (DataGridViewCell cell in row.Cells)
+                            sw.Write(cell.Value);
+                        sw.WriteLine();
+                    }
+                }
+                Process.Start("notepad.exe", saveFileDialog1.FileName);
+            }
+        }
+
+        private void btnExportOpen_Click(object sender, EventArgs e)
+        {
+            Microsoft.Office.Interop.Excel._Application excel = new Microsoft.Office.Interop.Excel.Application();
+            _Workbook workbook = excel.Workbooks.Add(Type.Missing);
+            _Worksheet worksheet = null;
+            try
+            {
+                worksheet = workbook.ActiveSheet;
+                worksheet.Name = "Inventory List";
+                for (int rowIndex = 0; rowIndex < dataGridView1.Rows.Count - 1; rowIndex++)
+                {
+                    for (int colIndex = 0; colIndex < dataGridView1.Columns.Count; colIndex++)
+                    {
+                        if (rowIndex == 0)
+                        {
+                            worksheet.Cells[rowIndex + 1, colIndex + 1] = dataGridView1.Columns[colIndex].HeaderText;
+                        }
+                        else
+                        {
+                            worksheet.Cells[rowIndex + 1, colIndex + 1] = dataGridView1.Rows[rowIndex].Cells[colIndex].Value.ToString();
+                        }
+                    }
+
+                }
+                if (saveFileDialog1.ShowDialog() == DialogResult.OK)
+                {
+                    workbook.SaveAs(saveFileDialog1.FileName);
+                    Process.Start("excel.exe", saveFileDialog1.FileName);
+                }
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            finally
+            {
+                excel.Quit();
+                workbook = null;
+                excel = null;
+
+            }
+        }
+
+        private void btnOpenWord_Click(object sender, EventArgs e)
+        {
+            Microsoft.Office.Interop.Word._Application word = new Microsoft.Office.Interop.Word.Application();
+            Document doc = word.Documents.Add();
+            Microsoft.Office.Interop.Word.Range rng = doc.Range(0, 0);
+            Table wdTable = doc.Tables.Add(rng, dataGridView1.Rows.Count, dataGridView1.Columns.Count);
+            wdTable.Borders.OutsideLineStyle = WdLineStyle.wdLineStyleDouble;
+            wdTable.Borders.InsideLineStyle = WdLineStyle.wdLineStyleSingle;
+            try
+            {
+                doc = word.ActiveDocument;
+                for (int i = 0; i < dataGridView1.Rows.Count - 1; i++)
+                {
+                    for (int j = 0; j < dataGridView1.Columns.Count; j++)
+                    {
+                        wdTable.Cell(i + 1, j + 1).Range.InsertAfter(dataGridView1.Rows[i].Cells[j].Value.ToString());
+                    }
+                }
+                if (saveFileDialog1.ShowDialog() == DialogResult.OK)
+                {
+                    doc.SaveAs(saveFileDialog1.FileName);
+                    Process.Start("winword.exe", saveFileDialog1.FileName);
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            finally
+            {
+                word.Quit();
+                word = null;
+                doc = null;
+
+            }
+        }
+    }
+}
---
+++ Attend  V 1.0.05/Attend/ManageReserv.Designer.cs
@@ -0,0 +1,320 @@
+﻿
+namespace Attend
+{
+    partial class ManageReserv
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
+            this.dateTimePicker2 = new System.Windows.Forms.DateTimePicker();
+            this.btnEE = new System.Windows.Forms.Button();
+            this.btnDelete = new System.Windows.Forms.Button();
+            this.btnAdd = new System.Windows.Forms.Button();
+            this.txtREN = new System.Windows.Forms.TextBox();
+            this.txtCID = new System.Windows.Forms.TextBox();
+            this.txtROT = new System.Windows.Forms.TextBox();
+            this.txtRON = new System.Windows.Forms.TextBox();
+            this.btnSearch = new System.Windows.Forms.Button();
+            this.cboSearch = new System.Windows.Forms.ComboBox();
+            this.txtSearch = new System.Windows.Forms.TextBox();
+            this.label10 = new System.Windows.Forms.Label();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.SFD2 = new System.Windows.Forms.SaveFileDialog();
+            this.bindingSource3 = new System.Windows.Forms.BindingSource(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.bindingSource3)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.label1.Location = new System.Drawing.Point(34, 23);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(123, 20);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Reserv Number:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.label2.Location = new System.Drawing.Point(83, 73);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(74, 20);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Client ID:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.label3.Location = new System.Drawing.Point(63, 122);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(94, 20);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Room Type:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.label4.Location = new System.Drawing.Point(41, 175);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(116, 20);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Room Number:";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.label5.Location = new System.Drawing.Point(400, 23);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(68, 20);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "Date IN:";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.label6.Location = new System.Drawing.Point(383, 73);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(85, 20);
+            this.label6.TabIndex = 5;
+            this.label6.Text = "Date OUT:";
+            // 
+            // dateTimePicker1
+            // 
+            this.dateTimePicker1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.dateTimePicker1.Location = new System.Drawing.Point(484, 23);
+            this.dateTimePicker1.Name = "dateTimePicker1";
+            this.dateTimePicker1.Size = new System.Drawing.Size(287, 26);
+            this.dateTimePicker1.TabIndex = 6;
+            // 
+            // dateTimePicker2
+            // 
+            this.dateTimePicker2.CalendarFont = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.dateTimePicker2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.dateTimePicker2.Location = new System.Drawing.Point(484, 67);
+            this.dateTimePicker2.Name = "dateTimePicker2";
+            this.dateTimePicker2.Size = new System.Drawing.Size(287, 26);
+            this.dateTimePicker2.TabIndex = 7;
+            // 
+            // btnEE
+            // 
+            this.btnEE.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.btnEE.Location = new System.Drawing.Point(529, 178);
+            this.btnEE.Name = "btnEE";
+            this.btnEE.Size = new System.Drawing.Size(192, 40);
+            this.btnEE.TabIndex = 123;
+            this.btnEE.Text = "Export As Excel File";
+            this.btnEE.UseVisualStyleBackColor = true;
+            this.btnEE.Click += new System.EventHandler(this.btnEE_Click);
+            // 
+            // btnDelete
+            // 
+            this.btnDelete.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.btnDelete.Location = new System.Drawing.Point(648, 119);
+            this.btnDelete.Name = "btnDelete";
+            this.btnDelete.Size = new System.Drawing.Size(93, 33);
+            this.btnDelete.TabIndex = 122;
+            this.btnDelete.Text = "Delete";
+            this.btnDelete.UseVisualStyleBackColor = true;
+            this.btnDelete.Click += new System.EventHandler(this.btnDelete_Click);
+            // 
+            // btnAdd
+            // 
+            this.btnAdd.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.btnAdd.Location = new System.Drawing.Point(516, 119);
+            this.btnAdd.Name = "btnAdd";
+            this.btnAdd.Size = new System.Drawing.Size(93, 33);
+            this.btnAdd.TabIndex = 121;
+            this.btnAdd.Text = "Add";
+            this.btnAdd.UseVisualStyleBackColor = true;
+            this.btnAdd.Click += new System.EventHandler(this.btnAdd_Click);
+            // 
+            // txtREN
+            // 
+            this.txtREN.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.txtREN.Location = new System.Drawing.Point(181, 25);
+            this.txtREN.Name = "txtREN";
+            this.txtREN.Size = new System.Drawing.Size(162, 26);
+            this.txtREN.TabIndex = 124;
+            // 
+            // txtCID
+            // 
+            this.txtCID.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.txtCID.Location = new System.Drawing.Point(181, 73);
+            this.txtCID.Name = "txtCID";
+            this.txtCID.Size = new System.Drawing.Size(162, 26);
+            this.txtCID.TabIndex = 125;
+            // 
+            // txtROT
+            // 
+            this.txtROT.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.txtROT.Location = new System.Drawing.Point(181, 122);
+            this.txtROT.Name = "txtROT";
+            this.txtROT.Size = new System.Drawing.Size(162, 26);
+            this.txtROT.TabIndex = 126;
+            // 
+            // txtRON
+            // 
+            this.txtRON.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.txtRON.Location = new System.Drawing.Point(181, 172);
+            this.txtRON.Name = "txtRON";
+            this.txtRON.Size = new System.Drawing.Size(162, 26);
+            this.txtRON.TabIndex = 127;
+            // 
+            // btnSearch
+            // 
+            this.btnSearch.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.btnSearch.Location = new System.Drawing.Point(234, 278);
+            this.btnSearch.Name = "btnSearch";
+            this.btnSearch.Size = new System.Drawing.Size(93, 33);
+            this.btnSearch.TabIndex = 133;
+            this.btnSearch.Text = "Search";
+            this.btnSearch.UseVisualStyleBackColor = true;
+            this.btnSearch.Click += new System.EventHandler(this.btnSearch_Click);
+            // 
+            // cboSearch
+            // 
+            this.cboSearch.FormattingEnabled = true;
+            this.cboSearch.Items.AddRange(new object[] {
+            "Reserv_ID",
+            "Client_ID",
+            "Date_IN",
+            "Date_OUT"});
+            this.cboSearch.Location = new System.Drawing.Point(47, 283);
+            this.cboSearch.Name = "cboSearch";
+            this.cboSearch.Size = new System.Drawing.Size(121, 21);
+            this.cboSearch.TabIndex = 132;
+            // 
+            // txtSearch
+            // 
+            this.txtSearch.Location = new System.Drawing.Point(113, 236);
+            this.txtSearch.Name = "txtSearch";
+            this.txtSearch.Size = new System.Drawing.Size(230, 20);
+            this.txtSearch.TabIndex = 131;
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.label10.Location = new System.Drawing.Point(43, 236);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(64, 20);
+            this.label10.TabIndex = 130;
+            this.label10.Text = "Search:";
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.AllCells;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(369, 236);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(550, 202);
+            this.dataGridView1.TabIndex = 134;
+            this.dataGridView1.CellEndEdit += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellEndEdit);
+            // 
+            // SFD2
+            // 
+            this.SFD2.Filter = "(*.xlsx)|*.xlsx|(*.txt)|*.txt|(*.docx)|*.docx";
+            // 
+            // bindingSource3
+            // 
+            this.bindingSource3.DataSource = typeof(Attend.MRE);
+            // 
+            // ManageReserv
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(939, 450);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.btnSearch);
+            this.Controls.Add(this.cboSearch);
+            this.Controls.Add(this.txtSearch);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.txtRON);
+            this.Controls.Add(this.txtROT);
+            this.Controls.Add(this.txtCID);
+            this.Controls.Add(this.txtREN);
+            this.Controls.Add(this.btnEE);
+            this.Controls.Add(this.btnDelete);
+            this.Controls.Add(this.btnAdd);
+            this.Controls.Add(this.dateTimePicker2);
+            this.Controls.Add(this.dateTimePicker1);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "ManageReserv";
+            this.Text = "Manage Reservation";
+            this.Load += new System.EventHandler(this.ManageReserv_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.bindingSource3)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.DateTimePicker dateTimePicker1;
+        private System.Windows.Forms.DateTimePicker dateTimePicker2;
+        private System.Windows.Forms.Button btnEE;
+        private System.Windows.Forms.Button btnDelete;
+        private System.Windows.Forms.Button btnAdd;
+        private System.Windows.Forms.TextBox txtREN;
+        private System.Windows.Forms.TextBox txtCID;
+        private System.Windows.Forms.TextBox txtROT;
+        private System.Windows.Forms.TextBox txtRON;
+        private System.Windows.Forms.Button btnSearch;
+        private System.Windows.Forms.ComboBox cboSearch;
+        private System.Windows.Forms.TextBox txtSearch;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.BindingSource bindingSource3;
+        private System.Windows.Forms.SaveFileDialog SFD2;
+    }
+}
